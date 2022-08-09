491b40d800de13c80e1c50d793e7866d4210cc80
MessKon <messiskon@gmail.com>
2022-08-09 10:38:26 +0100

    Hide reason from frontend failed login message

--- server.go
+++ server.go
@@ -121,9 +121,8 @@ func (s *server) authenticate(w http.ResponseWriter, r *http.Request) {
 				}
 			}
 			// TODO: Move this to the web server and make it prettier
-			msg := fmt.Sprintf("User '%s' failed authorization with reason: %s. "+
-				"Click <a href='%s'> here</a> to login again.", userInfo.GetName(),
-				reason, s.homepageURL)
+			msg := fmt.Sprintf("User '%s' failed authorization with reason: Unauthorized Access. "+
+				"Click <a href='%s'> here</a> to login again.", userInfo.GetName(), s.homepageURL)
 
 			returnHTML(w, http.StatusForbidden, msg)
 			return
